140a44fcf5069da9333421b9e1c1bb54ef4263f0
smokytek <filippo.casadei201112@gmail.com>
2026-08-23 21:15:29 +0200

    Translate immutable item templates safely

--- src/main/java/io/github/thebusybiscuit/exoticgarden/ItalianTranslations.java
+++ src/main/java/io/github/thebusybiscuit/exoticgarden/ItalianTranslations.java
@@ -94,13 +94,20 @@ final class ItalianTranslations {
             return false;
         }
 
-        ItemStack item;
+        ItemStack item = new ItemStack(slimefunItem.getItem());
         try {
-            item = (ItemStack) ITEM_STACK_TEMPLATE.get(slimefunItem);
+            if (!translate(item, translation)) {
+                return false;
+            }
+            ITEM_STACK_TEMPLATE.set(slimefunItem, item);
         } catch (IllegalAccessException ex) {
             return false;
         }
 
+        return true;
+    }
+
+    private static boolean translate(ItemStack item, Translation translation) {
         ItemMeta meta = item.getItemMeta();
         if (meta == null) {
             return false;
